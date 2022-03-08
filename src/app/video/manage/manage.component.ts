b56c163dd9d8b28c37f2dbd45496addbbe5a0979
rsl081 <53944109+rsl081@users.noreply.github.com>
2022-03-08 10:18:10 +0800

    rsl-routing

---
+++ src/app/video/manage/manage.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-manage',
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.css']
+})
+export class ManageComponent implements OnInit {
+
+  videOrder = '1'
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videOrder = params['sort'] === '2' ? params['sort'] : '1'
+    })
+  }
+
+  sort(event: Event) {
+    const { value } = (event.target as HTMLSelectElement)
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        sort: value
+      }
+    })
+
+  }
+
+}
