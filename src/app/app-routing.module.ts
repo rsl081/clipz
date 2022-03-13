c9453195b2c6af732f7f03c960ec86f7f5e74806
rsl081 <53944109+rsl081@users.noreply.github.com>
2022-03-13 13:00:43 +0800

    rsl-playing-videos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { async } from '@angular/core/testing';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ClipComponent } from './clip/clip.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ClipService } from './services/clip.service';
 
 const routes: Routes = [
   {
@@ -16,7 +18,14 @@ const routes: Routes = [
   },
   {
     path: 'clip/:id',
-    component: ClipComponent
+    component: ClipComponent,
+    resolve: {
+      clip: ClipService
+    }
+  },
+  {
+    path: '',
+    loadChildren: async() => (await import('./video/video.module')).VideoModule
   },
   {
     path: '**',
--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ClipService } from '../services/clip.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
-  styleUrls: ['./clips-list.component.css']
+  styleUrls: ['./clips-list.component.css'],
+  providers: [DatePipe]
 })
-export class ClipsListComponent implements OnInit {
+export class ClipsListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Input() scrollable = true
 
+  constructor(public clipService: ClipService) {
+    this.clipService.getClips()
+  }
+  
   ngOnInit(): void {
+    if(this.scrollable) {
+      window.addEventListener('scroll', this.handleScroll)
+    }
+  }
+  
+  handleScroll = () =>{
+
+    const { scrollTop, offsetHeight } = document.documentElement
+    const { innerHeight } = window
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
+
+    if(bottomOfWindow) {
+      this.clipService.getClips()
+    }
   }
 
+  ngOnDestroy(): void {
+    if(this.scrollable) {
+      window.removeEventListener('scroll', this.handleScroll)
+    }
+
+    this.clipService.pageClips = []
+  }
+
+  
 }
